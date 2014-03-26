88a974e4fc203330740fb51dc3d2936be90dd881
Rbn_3D <ruben3d3@gmail.com>
2014-03-26 19:40:40 +0100

    Ejercicio 2 prototype realizado

--- 2-objetos/prototype/js/ejercicio.js
+++ 2-objetos/prototype/js/ejercicio.js
@@ -1,4 +1,14 @@
 // Añade al objeto String el metodo espaciar() con la funcionalidad deseada
 
+String.prototype.espaciar = function () {
+	var str = "";
+
+	for (var i = 0; i < this.length; i++) {
+		str+= this.charAt(i) + " ";
+	};
+
+	return str.substring(0, str.length-1);
+}
+
 console.log('Esto es una prueba'.espaciar());
 console.log(String('Y esto es otra prueba').espaciar());
