a62b16e48957f688be8bccd910d77b838d05ef9d
NaxYo <nax@oxik.net>
2014-04-13 14:48:38 +0200

    Añaido segundo ejercicio de la segunda sesión del taller

---
+++ 2-objetos/herencia/js/VideoFilter.js
@@ -0,0 +1,79 @@
+var VideoFilter = function(videoSelector) {
+	this.video = $(videoSelector||'video');
+};
+
+VideoFilter.prototype.init = function() {
+	if(this.supportedVideoFormat()) {
+		var _this = this;
+		this.setCanvas();
+
+		(function gameLoop() {
+			var context = _this.canvas[0].getContext('2d');
+			_this.drawScreen.call(_this, context);
+			_this.applyFilter.call(_this, context);
+			requestAnimationFrame(gameLoop);
+		})();
+	}
+	else alert.log('No hay soporte de vídeo...');
+};
+
+VideoFilter.prototype.setCanvas = function() {
+	var container = this.video.parent();
+
+	this.canvas = $('<canvas>')
+		.attr('width', this.video.width())
+		.attr('height', this.video.height());
+	
+	this.video.after(this.canvas);
+};
+
+VideoFilter.prototype.drawScreen = function(context) {
+	var cw = this.canvas.width();
+	var ch = this.canvas.height();
+	var vInfo = this.videoCrop(cw, ch);
+
+	context.drawImage(this.video[0], vInfo.vx, vInfo.vy, vInfo.vw, vInfo.vh, 0, 0, cw, ch);
+};
+
+VideoFilter.prototype.videoCrop = function(cw, ch) {
+	var vw = this.video.width();
+	var vh = this.video.height();
+
+	var vx = 0;
+	var vy = 0;
+
+	var ratioC = cw/ch;
+	var ratioV = vw/vh;
+
+	if(ratioC>ratioV) {
+		var h = vw/ratioC;
+		vy = (vh-h)/2;
+		vh = h;
+	}
+	else {
+		var w = vh*ratioC;
+		vx = (vw-w)/2;
+		vw = w;
+	}
+
+	return { vx: vx, vy: vy, vw: vw, vh: vh};
+};
+
+VideoFilter.prototype.applyFilter = function(context) { };
+
+VideoFilter.prototype.supportedVideoFormat = function() {
+	if(this.canPlayType('video/webm')) return 'webm';
+	if(this.canPlayType('video/mp4'))  return 'mp4';
+	if(this.canPlayType('video/ogg'))  return 'ogg';
+};
+
+VideoFilter.prototype.canPlayType = function(type) {
+	return this.video[0].canPlayType(type)=='probably' || this.video[0].canPlayType(type)=='maybe';
+};
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame    ||
+	window.oRequestAnimationFrame      ||
+	window.msRequestAnimationFrame     ||
+	function(callback) { window.setTimeout(callback, 1000/60); };
--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Este repositorio pretende ser un punto de partida en el cual ir planteando ejerc
 * [Sesión #1: Introducción - Ambito de variables y funciones anónimas](https://github.com/NaxYo/js-0-100/tree/master/1-intro/scope "Ejercicio de la primer sesión: ambito de variables y funciones anónimas")
 * [Sesión #1: Introducción - Funciones, parámetros y más](https://github.com/NaxYo/js-0-100/tree/master/1-intro/function "Ejercicio de la primer sesión: funciones, parámetros y más")
 * [Sesión #2: Objetos - Prototipado de objetos](https://github.com/NaxYo/js-0-100/tree/master/2-objetos/prototype "Ejercicio de la segunda sesión: prototipado de objetos")
+* [Sesión #2: Objetos - Herencia de objetos e introducción de canvas&video tags](https://github.com/NaxYo/js-0-100/tree/master/2-objetos/herencia "Ejercicio de la segunda sesión: Herencia de objetos e introducción de canvas&video tags")
 
 ### Estructura
 
