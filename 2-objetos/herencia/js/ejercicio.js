2ece82ee9ab409ecf65536d6268f92072f40359d
Ruben <ruben3d3@gmail.com>
2014-04-23 10:48:05 +0200

    Ejercicio de herencia revisado y terminado

--- 2-objetos/herencia/js/ejercicio.js
+++ 2-objetos/herencia/js/ejercicio.js
@@ -2,6 +2,25 @@ var BWFilter = function() {
 
 };
 
+BWFilter.prototype = new VideoFilter();
+
+BWFilter.prototype.applyFilter = function(context) { 
+	
+	var cw = this.canvas.width();
+	var ch = this.canvas.height();
+
+	var imgd = context.getImageData(0, 0, cw, ch);
+
+	var pix = imgd.data;
+	for (var i = 0, n = pix.length; i < n; i += 4) {
+		var grayscale = pix[i  ] * .3 + pix[i+1] * .59 + pix[i+2] * .11;
+		pix[i  ] = grayscale;   // red
+		pix[i+1] = grayscale;   // green
+		pix[i+2] = grayscale;   // blue
+	}
+	context.putImageData(imgd, 0, 0);
+};
+
 /*
  * Implementa un filtro que herede de VideoFilter
  * y sobreescriba su metodo ApplyFilter generando
@@ -9,6 +28,6 @@ var BWFilter = function() {
  */
 
 $(function(){
-	var filter = new VideoFilter(); // new BWFilter();
+	var filter = new BWFilter();
 	filter.init();
 });
